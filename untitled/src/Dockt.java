ff6d5febdca638eabe55fabc27415c8bd61ab196
pixel50 <160168171+Pixel50@users.noreply.github.com>
2024-06-04 18:30:09 +0300

    error

---
+++ untitled/src/Dockt.java
@@ -0,0 +1,5 @@
+public class Dockt {
+    public Dockt(String name,String name2) {
+        System.out.println(name + " " + name2);
+    }
+}
--- untitled/src/Main.java
+++ untitled/src/Main.java
@@ -1,16 +1,35 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
 
-        StringBuilder sb = new StringBuilder();
+    @Override
+    public String toString() {
+        return "Main{}";
+    }
+
+    public static void main(String[] args) {
+        Dockt dockt = new Dockt("Hello", "World");
+
+        Scanner scanner = new Scanner(System.in);
+        List<String> list = new ArrayList<>();
+        HashMap<Integer, Dockt> map = new HashMap<>();
+        map.put(1, new Dockt("Hello", "World"));
+
+
         for (int i = 0; i < 10; i++) {
+            StringBuilder sb = new StringBuilder();
             sb.append("Hello World\n");
+            sb.append(map.get(i));
+
+            System.out.println("Enter your name:");
+            String name = scanner.nextLine();
+            list.add(name);
+            System.out.println("Hello " + name + "!");
+            System.out.println(list);
+
         }
-        System.out.println("Enter your name:");
-        String name = scanner.nextLine();
-        System.out.println("Hello " + name + "!");
-        System.out.println("Hello Java world! "+sb);
     }
 }
