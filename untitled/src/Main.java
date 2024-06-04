28f452e75c8cf886b41dab6a6db0a312e02d7be5
pixel50 <160168171+Pixel50@users.noreply.github.com>
2024-06-04 18:04:27 +0300

    +StringBulder

--- untitled/src/Main.java
+++ untitled/src/Main.java
@@ -4,9 +4,13 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < 10; i++) {
+            sb.append("Hello World\n");
+        }
         System.out.println("Enter your name:");
         String name = scanner.nextLine();
         System.out.println("Hello " + name + "!");
-        System.out.println("Hello Java world!");
+        System.out.println("Hello Java world! "+sb);
     }
 }
